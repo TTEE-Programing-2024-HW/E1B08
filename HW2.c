1be2401c0ff63395706b8122de08d4efec4d7e52
yangrongjhou <167836277+yangrongjhou@users.noreply.github.com>
2024-04-30 23:26:50 +0800

    加入程式的首頁和a.畫出直角三角形的程式

--- HW2.c
+++ HW2.c
@@ -2,8 +2,10 @@
  #include<stdlib.h> 
  int main(){
  	int state=0;
-	int a=0;
+	int a=0,i,j;
+	char ch,y;
 	int password;
+
 	printf("輸入密碼\n");
 	scanf("%d",&password);
 	while(1)
@@ -26,6 +28,50 @@
 			else if(a=3){
 				return 0;
 			}
+		case 1:
+			
+			printf("==========================\n");
+		   	printf("|| a.畫出直角三角形     ||\n");
+		   	printf("|| b.顯示乘法表         ||\n");
+		   	printf("|| c.結束               ||\n");
+		   	printf("==========================\n"); 
+		   	scanf(" %c",&ch);
+		   		switch(ch){
+		   			case 'a':
+		   			case 'A':
+		   				state=2;
+		   				break;
+					case 'b':
+					case 'B':
+						state=3;
+						break;
+					case 'c':
+					case 'C':
+					    state=4;
+						break;
+		   			
+				   }
+				system("CLS");
+		   		break;
+		case 2:
+			printf("請輸入a-n:");
+			   scanf(" %c",&ch);
+		    	for(i = 0;i<=ch-'a';i++)
+			   {
+			   	for(j = 0;j<=ch-'a';j++)
+			   	    {
+			   	   	
+					}
+					for(j = 0;j<=i;j++){
+						printf("%c",ch-j);
+					}
+					printf("\n");
+				   }
+				   system("PAUSE");
+		        system("CLS");
+		        state=1;
+		    	break;
+			
 	}
  
  	
