208332f4c36753cf0a781ec419dcc5d0100e5e67
yangrongjhou <167836277+yangrongjhou@users.noreply.github.com>
2024-05-10 21:43:20 +0800

    加入密碼程式，加入主畫面程式

--- HW3.c
+++ HW3.c
@@ -1,8 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	
-	
+	int state=0;
+	int a=0,i,j,y;
+	char ch;
+	int pass;
+	puts("      *            *            *");
+    puts("     ***          ***          ***");
+    puts("    *****        *****        *****");
+    puts("   *******      *******      *******");
+    puts("  *********    *********    *********");
+    puts("     |||          |||          |||");
+
+    puts("      *            *            *");
+    puts("     ***          ***          ***");
+    puts("    *****        *****        *****");
+    puts("   *******      *******      *******");
+    puts("  *********    *********    *********");
+    puts("     |||          |||          |||");
+
+    puts("      *            *            *");
+    puts("     ***          ***          ***");
+    puts("    *****        *****        *****");
+    puts("   *******      *******      *******");
+    puts("  *********    *********    *********");
+    puts(" ***********  ***********  ***********");
+    puts("     |||          |||          |||");
+	printf("輸入密碼\n");
+	scanf("%d",&pass);
+	while(1){
+		switch(state){
+			case 0:
+				if(pass==2024){
+					printf("密碼正確，歡迎\n");
+					system("pause");
+					system("CLS");
+					state=1;
+					break;
+				}
+				else if(pass!=2024&&a<2){
+					a=a+1;
+					printf("請重新輸入\n");
+					scanf("%d",&pass);
+					break;
+				}
+				else if(a==2){
+					return 0;
+				}
+		}
+	}
 	
 	system("pause");
 	return 0;
