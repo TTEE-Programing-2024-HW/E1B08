a721b6f32d89d2014c872936b178df5ac9021741
yangrongjhou <167836277+yangrongjhou@users.noreply.github.com>
2024-05-26 13:22:49 +0800

    加入輸入n值，並且輸入n位學生的資料

--- HW4.c
+++ HW4.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	int state=0;
-	int pass,a=0;
-	char ch;
-	puts("      *            *            *");
+
+struct data {
+    char name[10];
+    int ID;
+    int ma;
+    int ph;
+    int en;
+};
+
+void inputStudents(struct data *input, int n);
+
+int main() {
+    int state = 0;
+    int pass, a = 0, n;
+    char ch;
+    
+ 	puts("      *            *            *");
     puts("     ***          ***          ***");
     puts("    *****        *****        *****");
     puts("   *******      *******      *******");
@@ -25,69 +37,114 @@ int main(){
     puts("  *********    *********    *********");
     puts(" ***********  ***********  ***********");
     puts("     |||          |||          |||");
-	printf("輸入密碼\n");
-	scanf("%d",&pass);
-	while(1){
-		switch(state){
-			case 0:
-				if(pass==2024){
-					printf("密碼正確，歡迎\n");
-					system("pause");
+    printf("請輸入密碼：\n");
+    scanf("%d", &pass);
+
+    struct data input[10];
+
+    while (1) {
+        switch (state) {
+            case 0:
+                if (pass == 2024) {
+                    printf("密碼正確，歡迎\n");
+                    state = 1;
+                    system("pause");
+            		system("CLS");
+                    break;
+                } else if (a < 2) {
+                    a++;
+                    printf("請重新輸入密碼：\n");
+                    scanf("%d", &pass);
+                    break;
+                } else if (a == 2) {
+                    return 0;
+                }
+            case 1:
+                printf("=============================\n");
+                printf("|| a. 輸入學生成績         ||\n");
+                printf("|| b. 顯示學生成績         ||\n");
+                printf("|| c. 搜尋學生成績         ||\n");
+                printf("|| d. 成績排名             ||\n");
+                printf("|| e. 退出系統             ||\n");
+                printf("=============================\n");
+                scanf(" %c", &ch);
+                switch (ch) {
+                    case 'a':
+                    case 'A':
+                        state = 2;
+                        break;
+                    case 'b':
+                    case 'B':
+                        state = 3;
+                        break;
+                    case 'c':
+                    case 'C':
+                        state = 4;
+                        break;
+                    case 'd':
+                    case 'D':
+                        state = 5;
+                        break;
+                    case 'e':
+                    case 'E':
+                        state = 6;
+                        break;
+                    default:
+                        printf("輸入錯誤!!!\n");
+                        break;
+                }
+                break;
+            case 2:
+            	system("pause");
+            	system("CLS");
+                printf("請輸入學生資訊：\n");
+                printf("請輸入n(5~10)筆資料：\n");
+                scanf("%d", &n);
+                if (n >= 5 && n <= 10) {
+                    inputStudents(input, n);
+                    system("pause");
 					system("CLS");
-					state=1;
-					break;
-				}
-				else if(pass!=2024&&a<2){
-					a=a+1;
-					printf("請重新輸入\n");
-					scanf("%d",&pass);
-					break;
-				}
-				else if(a==2){
-					return 0;
-				}
-			case 1:
-				printf("==========================\n");
-		   		printf("|| a. Enter student gra     ||\n");
-		   		printf("|| b. Display student gra   ||\n");
-		   		printf("|| c. Sarch for student grad||\n");
-		   		printf("|| d. Grade ran             ||\n");
-		   		printf("|| e. Exit system           ||\n");
-
-		   		printf("==========================\n");
-		   		scanf("%s",&ch); 
-		   		switch(ch){
-		   			case 'a':
-		   			case 'A':
-		   				state=2;
-		   				break;
-		   			case 'b':
-		   			case 'B':
-		   				state=3;
-		   				break;
-		   			case 'c':
-		   			case 'C':
-		   				state=4;
-		   				break;
-		   			case 'd':
-		   			case 'D':
-		   				state=5;
-		   				break;
-		   			case 'e':
-		   			case 'E':
-		   				state=5;
-		   				break;
-		   			default:
-		   				printf("輸入錯誤!!!\n");
-		   				system("pause");
-		   				state=1;
-		   				break;
-				   }
-				   system("CLS");
-				   break;
-			}
-	}
-system("pause");
-
-return 0;
+                    state = 1;
+                    break;
+                } else {
+                    printf("輸入錯誤！n必須在5到10之間。\n");
+                    break;
+                }
+        }
+    }
+    system("pause");
+    return 0;
 }
+
+void inputStudents(struct data *input, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("請輸入第%d位學生的資訊：\n", i + 1);
+        printf("姓名：");
+        scanf("%s", input[i].name);
+        printf("學號：");
+        scanf("%d", &input[i].ID);
+        do {
+            printf("數學成績：");
+            scanf("%d", &input[i].ma);
+            if (input[i].ma < 0 || input[i].ma > 100) {
+                printf("成績輸入錯誤！請重新輸入。\n");
+            }
+        } while (input[i].ma < 0 || input[i].ma > 100);
+        do {
+            printf("物理成績：");
+            scanf("%d", &input[i].ph);
+            if (input[i].ph < 0 || input[i].ph > 100) {
+                printf("成績輸入錯誤！請重新輸入。\n");
+            }
+        } while (input[i].ph < 0 || input[i].ph > 100);
+        do {
+            printf("英文成績：");
+            scanf("%d", &input[i].en);
+            if (input[i].en < 0 || input[i].en > 100) {
+                printf("成績輸入錯誤！請重新輸入。\n");
+            }
+        } while (input[i].en < 0 || input[i].en > 100);
+    }
+}
+
